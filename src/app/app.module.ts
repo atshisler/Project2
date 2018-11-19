efbc310e9579130748d1ebe09bb00bd30bea2b55
Crystal Moutoussamy <zackifrass@gmail.com>
2018-11-19 10:01:31 -0500

    First Comment Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { PlatformPageComponent } from './platform-page/platform-page.component';
+import { GenrePageComponent } from './genre-page/genre-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlatformPageComponent,
+    GenrePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class YourAppModule{
+  
+}
