a599dc08a7af3814af0664a342330e91acf908f9
Alex Shisler <atshisler@gmail.com>
2018-11-19 23:13:14 -0500

    Able to retrieve info from IGDB

--- Project2/src/main/java/com/revature/igdb/IgdbRequest.java
+++ Project2/src/main/java/com/revature/igdb/IgdbRequest.java
@@ -1,8 +1,12 @@
 package com.revature.igdb;
 
+
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.revature.model.Game;
 
@@ -16,23 +20,27 @@ public class IgdbRequest {
 	private String key = "c7b9a8a0deb857507ca18673690dd26b";
 	private IGDBWrapper wrapper = new IGDBWrapper(key, Version.STANDARD, true);
 	private JSONArray gameInfo;
-	
+
 	public Game getGameByTitle(String title) {
 		gameInfo = null;
-		Parameters params = new Parameters().addSearch(title).addFields("name").addExpand("genres,developers,platforms").addLimit("5");
-		
+		Parameters params = new Parameters().addSearch(title).addFields("name,first_release_date")
+				.addExpand("genres,developers,platforms").addLimit("5");
+
 		wrapper.games(params, new OnSuccessCallback() {
 			public void onSuccess(JSONArray result) {
 				// JSONArray containing 2 titles
 				System.out.println("Success!");
-				System.out.println(result.toString());
 				gameInfo = result;
+				for (int i = 0; i < result.length(); i++)
+					System.out.println(result.get(i).toString());
+				createGame(gameInfo);
 			}
+
 			public void onError(Exception error) {
 				System.out.println("Error");// Do something on error
 			}
 		});
-		
+
 		return null;
 	}
 
@@ -61,7 +69,37 @@ public class IgdbRequest {
 
 //-----------------------Helper Methods-------------------------//
 	private void createGame(JSONArray result) {
-		
+		List<Game> newGames = new ArrayList<Game>();
+		for (int i = 0; i < result.length(); i++) {
+			Game newGame = new Game();
+			if(result.getJSONObject(i).has("name"))
+				newGame.setTitle(result.getJSONObject(i).getString("name"));
+			newGame.setCompany(getName(result.getJSONObject(i), "developers"));
+			newGame.setPlatform(getName(result.getJSONObject(i), "platforms"));
+			newGame.setGenre(getName(result.getJSONObject(i), "genres"));
+			if (result.getJSONObject(i).has("first_release_date")) {
+				Date date= new Date ((Long)result.getJSONObject(i).get("first_release_date"));
+				newGame.setReleaseDate(date);
+			}
+			newGames.add(newGame);
+			System.out.println(newGame.toString());
+
+		} // result array
+		for(int x = 0; x < newGames.size(); x++)
+			System.out.println(newGames.get(x));
+	}// createGame
+
+	//returns name value of a given key
+	private String getName(JSONObject result, String data) {
+		if (result.has(data)) {
+			JSONArray array = (JSONArray) result.get(data);
+			for (int x = 0; x < array.length(); x++) {
+				return array.getJSONObject(x).getString("name");
+			} // developer array
+		} // getDeveloper name if it has one
+		else
+			System.out.println("N/A");
+		return "N/A";
 	}
 
 	// ---------------Helper Methods End---------------------//
--- Project2/src/test/java/Driver.java
+++ Project2/src/test/java/Driver.java
@@ -1,13 +1,15 @@
 import com.revature.dao.UserDAO;
 import com.revature.dao.UserDAOImpl;
-import com.revature.model.GameUser;
+import com.revature.igdb.IgdbRequest;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		UserDAO ud = new UserDAOImpl();
+		IgdbRequest igd = new IgdbRequest();
 		//ud.createUser(new GameUser("G-Man", "halflife3@gmail.com", "Freeman", "user"));
 		System.out.println("Test: " + ud.getUser("G-Man"));
+		igd.getGameByTitle("Zelda");
 	}
 	
 
