efb21ba837344553d78d4dae4b95af5506ff52b4
Alex Shisler <atshisler@gmail.com>
2018-11-23 15:44:29 -0500

    update

--- Project2/src/main/java/com/revature/igdb/IgdbRequest.java
+++ Project2/src/main/java/com/revature/igdb/IgdbRequest.java
@@ -1,9 +1,11 @@
 package com.revature.igdb;
 
-
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,35 +19,45 @@ import wrapper.Parameters;
 import wrapper.Version;
 
 public class IgdbRequest {
-	//private String key = "c7b9a8a0deb857507ca18673690dd26b";
+	// private String key = "c7b9a8a0deb857507ca18673690dd26b";
 	private String key = "b7f579b4334207852addd702fe0e3442";
 	private IGDBWrapper wrapper = new IGDBWrapper(key, Version.STANDARD, true);
 	private JSONArray gameInfo;
 	private List<Game> gameList;
 	boolean success = false;
-	public List<Game> getGameByTitle(String title) {
+	private ExecutorService executor = Executors.newSingleThreadExecutor();
+
+	public Future<List<Game>> getGameByTitle(String title) {
 		gameInfo = null;
-		Parameters params = new Parameters().addSearch(title).addFields("name,first_release_date");
-				//.addExpand("genres,developers,platforms").addLimit("5");
-
-		wrapper.games(params, new OnSuccessCallback() {
-			public void onSuccess(JSONArray result) {
-				// JSONArray containing 2 titles
-				System.out.println("Success!");
-				gameInfo = result;
-				//for (int i = 0; i < result.length(); i++)
-				//	System.out.println(result.get(i).toString());
-				success = true;
-				gameList = createGame(gameInfo);
-				
-			}
-
-			public void onError(Exception error) {
-				System.out.println("Error");// Do something on error
-			}
-		});
+		success = false;
 		
-		return gameList;
+		// .addExpand("genres,developers,platforms").addLimit("5");
+		return executor.submit(() -> {
+			Parameters params = new Parameters().addSearch(title).addFields("name,first_release_date");
+			wrapper.games(params, new OnSuccessCallback() {
+				public void onSuccess(JSONArray result) {
+					// JSONArray containing 2 titles
+					System.out.println("Success!");
+					gameInfo = result;
+					// for (int i = 0; i < result.length(); i++)
+					// System.out.println(result.get(i).toString());
+
+					success = true;
+					gameList = createGame(gameInfo);
+
+				}
+
+				public void onError(Exception error) {
+					System.out.println("Error");// Do something on error
+				}
+			});
+			return gameList;
+		});
+
+	}
+
+	public void setGameList(String title) {
+
 	}
 
 	public List<Game> getGamesByName() {
@@ -71,32 +83,41 @@ public class IgdbRequest {
 
 	}
 
+	public List<Game> getResponse() {
+		while (!success)
+			System.out.println("waiting");
+		success = false;
+		return gameList;
+
+	}
+
 //-----------------------Helper Methods-------------------------//
 	private List<Game> createGame(JSONArray result) {
 		List<Game> newGames = new ArrayList<Game>();
 		for (int i = 0; i < result.length(); i++) {
 			Game newGame = new Game();
-			if(result.getJSONObject(i).has("name"))
-				newGame.setTitle(result.getJSONObject(i).getString("name"));
-			newGame.setCompany(getName(result.getJSONObject(i), "developers"));
-			newGame.setPlatform(getName(result.getJSONObject(i), "platforms"));
-			newGame.setGenre(getName(result.getJSONObject(i), "genres"));
-			if (result.getJSONObject(i).has("first_release_date")) {
-				Date date= new Date ((Long)result.getJSONObject(i).get("first_release_date"));
-				newGame.setReleaseDate(date);
-			}
+			// if(result.getJSONObject(i).has("name"))
+			newGame.setTitle(result.getJSONObject(i).getString("name"));
+			// newGame.setCompany(getName(result.getJSONObject(i), "developers"));
+			// newGame.setPlatform(getName(result.getJSONObject(i), "platforms"));
+			// newGame.setGenre(getName(result.getJSONObject(i), "genres"));
+			// if (result.getJSONObject(i).has("first_release_date")) {
+			// Date date= new Date
+			// ((Long)result.getJSONObject(i).get("first_release_date"));
+			// newGame.setReleaseDate(date);
+			// }
 			newGames.add(newGame);
-			//System.out.println(newGames.size() + " Size end " + newGames.toString());
-
+			// System.out.println(newGames.size() + " Size end " + newGames.toString());
+			//System.out.println(newGames);
 		} // result array
-		//for(int x = 0; x < newGames.size(); x++)
-		//	System.out.println(newGames.get(x));
+			// for(int x = 0; x < newGames.size(); x++)
+			// System.out.println(newGames.get(x));
 
 		return newGames;
-			
+
 	}// createGame
 
-	//returns name value of a given key
+	// returns name value of a given key
 	private String getName(JSONObject result, String data) {
 		if (result.has(data)) {
 			JSONArray array = (JSONArray) result.get(data);
--- Project2/src/test/java/Driver.java
+++ Project2/src/test/java/Driver.java
@@ -1,4 +1,6 @@
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import com.revature.dao.UserDAO;
 import com.revature.dao.UserDAOImpl;
@@ -7,17 +9,36 @@ import com.revature.model.Game;
 
 public class Driver {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		UserDAO ud = new UserDAOImpl();
 		IgdbRequest igd = new IgdbRequest();
-		List<Game> gameList;
+		Future<List<Game>> futureList;
 		//ud.createUser(new GameUser("G-Man", "halflife3@gmail.com", "Freeman", "user"));
 		//System.out.println("Test: " + ud.getUser("G-Man"));
 		
-		//System.out.println(igd.getGameByTitle("Zelda"));
+		futureList = igd.getGameByTitle("Zelda");
 		
-		//for(int i = 0; i < gameList.size(); i++)
-		//	System.out.println("Games: " + gameList.get(i));
+		
+		while(!futureList.isDone()) {
+			System.out.println("Future");
+			
+		}
+		if(futureList.isDone())
+			System.out.println("done");
+		System.out.println(futureList.get());
+		List<Game> gameList = futureList.get();
+		
+		for(int i = 0; i < gameList.size(); i++)
+			System.out.println("Games: " + gameList.get(i));
+		
+		
+		futureList = igd.getGameByTitle("Dark Souls");	
+		gameList = futureList.get();
+		
+		System.out.println(gameList);
+				
+		for(int i = 0; i < gameList.size(); i++)
+			System.out.println("Games: " + gameList.get(i));
 		
 	}
 	
