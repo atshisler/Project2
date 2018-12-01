a556d9de9a4f817613571fca28016a4853a61335
crymou <44808473+crymou@users.noreply.github.com>
2018-12-01 17:18:53 -0500

    searching

--- angular-project2/src/app/search/search.service.ts
+++ angular-project2/src/app/search/search.service.ts
@@ -7,10 +7,12 @@ import { HttpClient } from '@angular/common/http';
 export class SearchService {
 
   private _url: string ='http://localhost:8080/name-and-game/game/search/zelda';
+  search: string = '';
 
   constructor(private http: HttpClient) { }
 
   getGames () {
+    this._url = `this._url${this.search}`;
     return this.http.get(this._url);
   }
 }
