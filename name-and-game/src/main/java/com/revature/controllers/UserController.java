1d3ea63670fbe5f43043cf8ad3c9670f83dd75f7
Alex Shisler <atshisler@gmail.com>
2018-12-02 12:40:06 -0500

    added user controller with login

---
+++ name-and-game/src/main/java/com/revature/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.revature.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.revature.dao.UserDAO;
+import com.revature.dao.UserDAOImpl;
+import com.revature.model.GameUser;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+private UserDAO ud = new UserDAOImpl();
+
+
+@PostMapping("/login")
+@ResponseBody
+public GameUser login(@RequestParam("email") String user, @RequestParam("password")String pass) {
+    return ud.login(user, pass);
+}
+
+}
